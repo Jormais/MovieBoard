1f407e3d98d7283b0d11390cf7d62105b39b2077
Jonay Brito Hernández <jonaybrito15@gmail.com>
2021-02-28 17:02:58 +0000

    Added search bar
    A search bar has been added and the appropriate functionality has been added to allow you to search the film table.

--- MovieBoard/MovieBoard/MoviesViewController.swift
+++ MovieBoard/MovieBoard/MoviesViewController.swift
@@ -9,34 +9,64 @@ import UIKit
 
 class MovieViewController : UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating {
     
-    var array : [String] = ["Cellda1","Cellda2","Cellda3"]
+    var films : [String] = ["Cellda1","Cellda2","Cellda3"]
+    var filteredFilms : [String] = []
+    
+    var isSearchBarEmpty: Bool {
+      return searchController.searchBar.text?.isEmpty ?? true
+    }
+    var isFiltering: Bool {
+      return searchController.isActive && !isSearchBarEmpty
+    }
+    let searchController = UISearchController(searchResultsController: nil)
+    
     @IBOutlet var movieTable : UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.movieTable.delegate = self
         self.movieTable.dataSource = self
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search Films"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard segue.identifier == "toDetail", let indexPath = movieTable.indexPathForSelectedRow, let detailController = segue.destination as? MovieDetailViewController
         else {return}
         
-        detailController.titleCell = array[indexPath.row]
+        detailController.titleCell = films[indexPath.row]
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return array.count
+        if isFiltering{
+            return filteredFilms.count
+        }
+        return films.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) 
-        cell.textLabel?.text = array[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if isFiltering{
+            cell.textLabel?.text = filteredFilms[indexPath.row]
+            return cell
+        }
+        cell.textLabel?.text = films[indexPath.row]
         return cell
     }
     
     func updateSearchResults(for searchController: UISearchController) {
-        
+           let searchBar = searchController.searchBar
+           filterForSearchText(searchBar.text!)
+       }
+    
+    func filterForSearchText(_ searchText: String) { 
+        filteredFilms = films.filter({ (film) -> Bool in
+            return film.lowercased().contains(searchText.lowercased())
+        })
+        self.movieTable.reloadData()
     }
     
 }
