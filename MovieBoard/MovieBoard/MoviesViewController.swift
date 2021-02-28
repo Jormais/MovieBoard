dff772959fbf10558a806f8ef04f97bb518fac3c
Jonay Brito Hernández <jonaybrito15@gmail.com>
2021-02-28 15:45:07 +0000

    Added navigation
    Added the delegate and data source in the table as well as the detail view screen and a segue for it.

--- MovieBoard/MovieBoard/MoviesViewController.swift
+++ MovieBoard/MovieBoard/MoviesViewController.swift
@@ -7,29 +7,31 @@
 
 import UIKit
 
-
-let appDelegate = UIApplication.shared.delegate as? AppDelegate
-
 class MovieViewController : UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating {
     
+    var array : [String] = ["Cellda1","Cellda2","Cellda3"]
     @IBOutlet var movieTable : UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.movieTable.delegate = self
+        self.movieTable.dataSource = self
     }
     
-   
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       
+        return array.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) 
+        cell.textLabel?.text = array[indexPath.row]
+        return cell
     }
     
     func updateSearchResults(for searchController: UISearchController) {
-        <#code#>
+        
     }
     
 }
